06cbc81574211b579e1e11a7daa24a795fea3561
rafa <rafaelsistem87@gmail.com>
2021-06-04 09:20:34 -0700

    fix NPM problems

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+
+import TestComponent from "./atoms/TestComponent";
+import ButtonComponent from "./atoms/ButtonComponent"
+
+export { TestComponent, ButtonComponent };
+
+/* use in this way create NPM problems
 export * as ButtonComponent from "./atoms/ButtonComponent";
-export * as TestComponent from "./atoms/TestComponent";
+export * as TestComponent from "./atoms/TestComponent"; */
